263bfc05030ac064b2855d524100cf9a38cd0154
RillSvai <kirilkvas07@gmail.com>
2023-10-24 20:32:54 +0300

    add relationalships to models

--- PokemonReview_API/Models/Pokemon.cs
+++ PokemonReview_API/Models/Pokemon.cs
@@ -5,5 +5,8 @@
 		public int Id { get; set; }
 		public string Name { get; set; }
 		public DateTime BirthDate { get; set; }
+		public IEnumerable<Review> Reviews { get; set; }
+		public IEnumerable<PokemonCategory> PokemonCategories { get; set; }	
+		public IEnumerable<PokemonOwner> PokemonOwners { get; set; }
 	}
 }
